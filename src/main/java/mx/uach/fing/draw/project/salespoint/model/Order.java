2d5adf32b0c3b5b8dc8737c2a91ad82e32635780
Luis <Frost_Leviathan@hotmail.com>
2015-09-14 11:45:53 -0600

    Se agregaron los modelos del proyecto.

---
+++ src/main/java/mx/uach/fing/draw/project/salespoint/model/Order.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2015 Luis Chávez Bustamante
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mx.uach.fing.draw.project.salespoint.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author Luis Chávez Bustamante
+ */
+@Entity
+@Table(name = "orders")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private Long orderId;
+    
+    @Column(name = "created_at")
+    private Date createdAt;
+    
+    @Column(name = "description")
+    private String description;
+    
+    @Column(name = "status")
+    private String status;
+
+    @OneToMany(targetEntity = User.class)
+    private User user;
+    
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
