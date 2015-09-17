b87307b768d6cdcc5d76606d17dd4f4584628f83
Luis <Frost_Leviathan@hotmail.com>
2015-09-16 21:13:03 -0600

    Se agregaron las rutas de la aplicacion.

--- src/main/java/mx/uach/fing/draw/project/salespoint/model/SaleOrder.java
+++ src/main/java/mx/uach/fing/draw/project/salespoint/model/SaleOrder.java
@@ -18,12 +18,14 @@ package mx.uach.fing.draw.project.salespoint.model;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
 import org.hibernate.annotations.GenericGenerator;
 
 /**
